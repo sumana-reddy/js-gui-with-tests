e2adf47d51d2de1095cdd3423503b9464ee728f7
sumanareddy <sumanareddy.guc27@gmail.com>
2020-03-31 11:59:13 -0500

    last commit

--- test/main.test.js
+++ test/main.test.js
@@ -1,5 +1,10 @@
 QUnit.module('MAIN MODULE', {})  // group all these tests together
 
+QUnit.test( "hello test", function( assert ) {
+  assert.ok( 1 == "1", "Passed!" );
+  assert.ok( "1" === "1", "Passed!" );
+})
+
 QUnit.test('TEST add', assert => {
   assert.equal(add(1, 1), 2, 'Positive integers')
   assert.equal(add(-1, -1), -2, 'Negative integers')
